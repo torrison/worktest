2cc6c069a8468e3f034616d4dc3e4925e4d4c88f
torrison <cd99@mail.ru>
2017-02-01 03:55:26 +0200

    Update

--- application/views/outside/pages/index.php
+++ application/views/outside/pages/index.php
@@ -2,15 +2,11 @@
     <div class="wblock main_block">
 
         <div class="row">
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xlg-4">
-                <img alt="Why1" src="https://www.recruiter.com/i/wp-content/uploads/2014/08/Multiethnic-People-with-Startup.jpg" class="why_img" />
+            <div class="col-md-12">
+                <a href="http://sandbox1.esy.es/yii/web/">Yii Framework 2 (Yii2)</a><br>
+                <a href="http://sandbox1.esy.es/ci3/welcome/">Code Igniter 3 (CI3)</a><br>
             </div>
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xlg-4">
-                <img alt="Why2" src="https://www.recruiter.com/i/wp-content/uploads/2014/08/Multiethnic-People-with-Startup.jpg" class="why_img" />
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xlg-4">
-                <img alt="Why3" src="https://www.recruiter.com/i/wp-content/uploads/2014/08/Multiethnic-People-with-Startup.jpg" class="why_img" />
-            </div>
+
         </div>
     </div>
 
